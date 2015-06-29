aa0c4a1634af9cff23bc272eb27dd10809c45fd6
Theeradech <notegamer@hotmail.com>
2015-06-29 23:31:51 +0700

    no message

---
+++ src/Ngcode/PermissionUi/PermissionUiServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Ngcode\PermissionUi;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionUiServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('ngcode/permission-ui');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
